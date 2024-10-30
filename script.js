a6cdf794e1068f583ce56945d3494909b12ed37c
QueenSarah21 <128749996+Queen-Sarah21@users.noreply.github.com>
2024-10-30 16:14:58 -0400

    so far so good

--- script.js
+++ script.js
@@ -6,20 +6,3 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
-function toggleTheme() {
-    const body = document.body;
-    const themeToggleIcon = document.getElementById("theme-toggle");
-
-    // Toggle dark theme class
-    body.classList.toggle("dark-theme");
-
-    // Change icon based on the theme
-    if (body.classList.contains("dark-theme")) {
-        themeToggleIcon.src = "assets/brightness.png"; // Change to brightness icon
-    } else {
-        themeToggleIcon.src = "assets/moon.png"; // Change back to moon icon
-    }
-}
-
-// Add event listener to the theme toggle icon
-document.getElementById("theme-toggle").addEventListener("click", toggleTheme);
