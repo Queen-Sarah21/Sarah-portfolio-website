dc2a26b9becb1f006b8495dee35515933d707ab1
QueenSarah21 <128749996+Queen-Sarah21@users.noreply.github.com>
2024-11-02 10:36:21 -0400

    adjustment of project section styling

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
+function openModal() {
+    document.getElementById("descriptionModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("descriptionModal").style.display = "none";
+}
+
+// Optional: Close the modal when clicking outside of it
+window.onclick = function(event) {
+    const modal = document.getElementById("descriptionModal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
